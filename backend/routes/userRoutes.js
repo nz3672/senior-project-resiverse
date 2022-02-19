041d95feaf246210d88692d8912622540cabd692
telio-s <telio.s@hotmail.com>
2022-02-19 18:52:01 +0700

    route, controller for user

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getUsers,
+  setUsers,
+  updateUsers,
+  deleteUsers,
+} = require("../controllers/userController");
+
+router.route("/").get(getUsers).post(setUsers);
+router.route("/:id").put(updateUsers).delete(deleteUsers);
+
+// router.get("/", getUsers);
+
+// router.post("/", setUsers);
+
+// router.put("/:id", updateUsers);
+
+// router.delete("/:id", deleteUsers);
+
+module.exports = router;
